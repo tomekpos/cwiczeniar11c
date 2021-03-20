7c901e345342dad9734e706de6bb29e53e583134
your username <vorexxx@gmail.com>
2021-03-20 16:30:33 +0100

    add R project and download data from weather API

---
+++ first_script.R
@@ -0,0 +1,10 @@
+install.packages("httr")
+install.packages("jsonlite")
+
+library(httr)
+require(jsonlite)
+
+endpoint<-"https://api.openweathermap.org/data/2.5/weather?q=Warszawa&appid=1765994b51ed366c506d5dc0d0b07b77"
+getWeather<-GET(endpoint)
+weatherText<-content(getWeather,as="text")
+weatherJson<-fromJSON(weatherText,flatten = FALSE)
